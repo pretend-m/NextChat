88e63e10255557d475428facb1f9b122c67429c4
pretend <1908206972@qq.com>
2025-08-12 15:07:29 +0800

    更新

--- app/constant.ts
+++ app/constant.ts
@@ -472,51 +472,24 @@ export const VISION_MODEL_REGEXES = [
 export const EXCLUDE_VISION_MODEL_REGEXES = [/claude-3-5-haiku-20241022/];
 
 const openaiModels = [
-  "gpt-3.5-turbo",
-  "gpt-3.5-turbo-0125",
-  "gpt-3.5-turbo-0301",
-  "gpt-3.5-turbo-0613",
-  "gpt-3.5-turbo-1106",
   "gpt-3.5-turbo-16k",
-  "gpt-3.5-turbo-16k-0613",
-  "gpt-3.5-turbo-instruct",
-  "gpt-3.5-turbo-instruct-0914",
   "gpt-4",
-  "gpt-4-0125-preview",
-  "gpt-4-0613",
-  "gpt-4-1106-preview",
-  "gpt-4-turbo",
   "gpt-4-turbo-2024-04-09",
-  "gpt-4-turbo-preview",
+  "gpt-4-all",
   "gpt-4.1",
   "gpt-4.1-2025-04-14",
   "gpt-4.1-mini",
   "gpt-4.1-mini-2025-04-14",
   "gpt-4.1-nano",
   "gpt-4.1-nano-2025-04-14",
-  "gpt-4.5-preview",
-  "gpt-4.5-preview-2025-02-27",
   "gpt-4o",
-  "gpt-4o-2024-05-13",
-  "gpt-4o-2024-08-06",
   "gpt-4o-2024-11-20",
   "gpt-4o-mini",
   "gpt-4o-mini-2024-07-18",
-  "o1",
-  "o1-2024-12-17",
-  "o1-mini",
-  "o1-mini-2024-09-12",
-  "o1-preview",
-  "o1-preview-2024-09-12",
-  "chatgpt-4o-latest",
-  "o3",
-  "o3-2025-04-16",
-  "o3-mini",
-  "o3-mini-2025-01-31",
-  "o4-mini",
-  "o4-mini-2025-04-16",
-  "dall-e-2",
-  "dall-e-3",
+  "gpt-5",
+  "gpt-5-2025-08-07",
+  "gpt-5-mini-2025-08-07",
+  "gpt-5-nano-2025-08-07"
 ];
 
 // const googleModels = [
@@ -619,11 +592,10 @@ const openaiModels = [
 // ];
 
 const deepseekModels = [
-  "deepseek-r1",
+  "deepseek-r1-250528",
   "deepseek-r1-search",
   "deepseek-v3",
-  "deepseek-v3-search",
-  "deepseek-reasoner",
+  "deepseek-v3-search"
 ];
 
 // const xAIModes = [
